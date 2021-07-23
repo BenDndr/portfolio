96d214f1fa8acd2718420e4377facd145a90eef8
Benjamin DANDRE <dandre.ben@gmail.com>
2021-07-23 14:15:48 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import Counter from './components/Counter';
+import Projects from './components/Projects';
 
 function App() {
+  const [projects, setProject] = useState([
+    {
+        id: 1,
+        name: 'Test',
+        description: 'Hello'
+    },
+    {
+        id: 2,
+        name: 'React',
+        description: 'Good'
+    },
+  ])
+
+  const [count, setCount] = useState(1)
+  
+  const addOne = (count) => {
+    setCount(count += 1)  
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Benjamin Dandre</h1>
+        <i className="fa fa-spinner fa-spin"></i>
+        <Counter count={count} click={addOne}/> 
+      </div>
+
+      <div className="container project-section">
+        <Projects projects={projects}/>
+      </div>
     </div>
   );
 }
